2236a6f1599e35325cb07cba3389428da081eddf
Roman <info@presstomsk.ru>
2021-06-29 21:43:43 +0700

    4?

--- HomeWork_CSharp/HomeWork_1.cs
+++ HomeWork_CSharp/HomeWork_1.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using System;
+using System.Linq;
 
 namespace HomeWork_CSharp
 {
@@ -11,7 +12,7 @@ namespace HomeWork_CSharp
             // ДЗ1.Написать программу, которая считывает символы с клавиатуры,
             //пока не будет введена точка.
             //Программа должна сосчитать количество введенных пользователем пробелов.
-            
+
             Console.WriteLine("------HomeWork_1------");
             Console.WriteLine();
             Console.Write("Введите символы с клавиатуры: ");
@@ -37,22 +38,22 @@ namespace HomeWork_CSharp
             Console.WriteLine("------HomeWork_2------");
             Console.WriteLine();
             Console.Write("Введите строку с клавиатуры: ");
-            int size=50;
-            char[] arr=new char[size];
+            int size = 50;
+            char[] arr = new char[size];
             counter = 0;
             do
             {
                 arr[counter] = Console.ReadKey().KeyChar;
                 counter++;
-               
+
             }
-            while ((counter!=arr.Length)&&(arr[counter - 1] != 13));
+            while ((counter != arr.Length) && (arr[counter - 1] != 13));
             Console.WriteLine();
-            for (int i = 0; i < counter-1; i++)
-            {               
-                
+            for (int i = 0; i < counter - 1; i++)
+            {
+
                 if ((arr[i] >= 97) && (arr[i] <= 122))
-                    arr[i] =(char)((int)arr[i]-32);
+                    arr[i] = (char)((int)arr[i] - 32);
                 else if ((arr[i] >= 65) && (arr[i] <= 90))
                     arr[i] = (char)((int)arr[i] + 32);
             }
@@ -94,8 +95,89 @@ namespace HomeWork_CSharp
                     Console.Write($"{A}\t");
                 }
                 Console.WriteLine();
-            }    
+            }
+            Console.WriteLine();
 
+            //ДЗ 4.Даны 2 массива размерности M и N соответственно.
+            //Необходимо переписать в третий массив общие элементы
+            //первых двух массивов без повторений.
+            /*
+            Console.WriteLine("------HomeWork_4------");
+            Console.WriteLine();
+            int[] arr1, arr2;
+            int[] arr1WithoutRepetitions, finalArr;
+            int M, N;
+            Console.Write("Введите размер первого массива: ");
+            M = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите размер второго массива: ");
+            N = Convert.ToInt32(Console.ReadLine());
+            arr1 = new int[M];
+            arr2 = new int[N];
+            Console.Write("Первый массив: ");
+            RandomArray(arr1);
+            PrintArray(arr1);
+            Console.Write("Второй массив: ");
+            RandomArray(arr2);
+            PrintArray(arr2);
+            bool flag = true;
+            arr1WithoutRepetitions = new int[NumberOfUniqueNumbersInArr(arr1)];
+            Console.WriteLine(NumberOfUniqueNumbersInArr(arr1));
+            /*counter = 0;
+            for (int i = 0; i < M; i++)
+            {   
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr1[i] == arr1[j])
+                    { 
+                        flag = false;
+                        break;
+                    }
+                    flag = true;
+                }
+                if (flag)
+                {
+                    arr1WithoutRepetitions[counter] = arr1[i];
+                    counter++;
+                }
+            }
+            PrintArray(arr1WithoutRepetitions);*/
         }
+        static void RandomArray(int[] array)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 10);
+            }
+        }
+        static void PrintArray(int[] array)
+        {
+            foreach (int item in array)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine();
+        }
+        static int NumberOfUniqueNumbersInArr(int[] array)
+        {
+            int max = array.Max();
+            int min = array.Min();
+            int numberOfUniqueNumbers = 0;
+            int[] arrOfFlags = new int[max - min + 1];
+            for (int i = 0; i < arrOfFlags.Length; i++)
+            {
+                for (int j = 0; j < array.Length; i++)
+                {
+                    if (arrOfFlags[array[j] - min] == 0) arrOfFlags[array[j] - min] += 1;
+                }
+            }
+            for (int i = 0; i < arrOfFlags.Length; i++)
+            {
+                numberOfUniqueNumbers += arrOfFlags[i];
+            }
+            return max;
+        }
+
+    
     }
 }
